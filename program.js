c3b097039e992598291187b624073c56434bba5b
Raptor <Raptor-S@outlook.jp>
2023-09-01 23:11:45 +0900

    フォルダーを選択して読み込む(中間)

--- program.js
+++ program.js
@@ -1,31 +1,267 @@
 function first_script() {
     document.getElementById("file").addEventListener("change", (ev) => {
-        for (let i = 0; i < ev.target.files.length; i++) {
-            let file = ev.target.files[i];
+        //for (let i = 0; i < ev.target.files.length; i++) {
+        i = 0;
+        let file = ev.target.files[i];
 
-            // ディレクトリの相対パス
-            let relativePath = file.webkitRelativePath;
+        // ファイルリーダーを作成する
+        let fileReader = new FileReader();
 
-            // ここではテキストファイルとして読み出してみる.
-            let fileReader = new FileReader();
-            fileReader.onload = (event) => {
-                // 内容を取得する.
-                let text = event.target.result;
+        // ファイルが読み込まれたら、次の処理を行う
+        fileReader.onload = (event) => {
+            // 画像要素を作成する
+            let img = document.createElement("img");
+            img.src = event.target.result;
+            // 画像が読み込まれたら
+            img.onload = () => {
+                // canvas要素を作成する
+                let canvas = document.createElement("canvas");
+                // canvasのサイズを画像と同じにする
+                // 修正箇所：img.widthとimg.heightを使う
+                var canvas_black = document.createElement("canvas");
+                var canvas_white = document.createElement("canvas");
+                var ctx_black = canvas_black.getContext("2d");
+                var ctx_white = canvas_white.getContext("2d");
+                canvas_black.width = image.width;
+                canvas_white.width = image.width;
+                canvas_black.height = image.height;
+                canvas_white.height = image.height;
+                canvas.width = img.width;
+                canvas.height = img.height;
+                // canvasのコンテキストを取得する
+                let ctx = canvas.getContext("2d");
+                // canvasに画像を描画する
+                ctx.drawImage(img, 0, 0);
+                var imgData_black = ctx_black.getImageData(
+                    0,
+                    0,
+                    canvas_black.width,
+                    canvas_black.height
+                );
+                var imgData_white = ctx_white.getImageData(
+                    0,
+                    0,
+                    canvas_white.width,
+                    canvas_white.height
+                );
+                // canvasの画像データを取得する
+                let imgd = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-                // 表示してみる.
-                //console.log(relativePath, text);
+                // 画像データのピクセル配列を取得する
+                let pix = imgd.data;
+                // ピクセル配列をループして白色のピクセルを透明にする
+                for (var i = 0; i < imgData_black.data.length; i += 4) {
+                    // RGBの値を取得
+                    var r = imgData_black.data[i];
+                    var g = imgData_black.data[i + 1];
+                    var b = imgData_black.data[i + 2];
 
-                // data64形式に変換する.
-                let data64 = btoa(unescape(encodeURIComponent(text)));
+                    //0は透明
 
-                // canvasまたはimgでhtml上に表示させる.
-                let img = document.createElement("img");
-                img.src = "data:image/png;base64," + data64;
-                document.body.appendChild(img);
+                    //黒レイヤーの操作
+                    //黒は黒くする 黒以外は透明にする
+
+                    //if (r >= threshold && g >= threshold && b >= threshold) {
+
+                    function euclidean(r1, g1, b1, r2, g2, b2) {
+                        return Math.sqrt(
+                            Math.pow(r2 - r1, 2) +
+                            Math.pow(g2 - g1, 2) +
+                            Math.pow(b2 - b1, 2)
+                        );
+                    }
+                    //白の値は360付近
+
+                    let base_rgb = [51, 16, 36];
+
+                    let shikisa = euclidean(
+                        imgData_black.data[i],
+                        imgData_black.data[i + 1],
+                        imgData_black.data[i + 2],
+                        base_rgb[0],
+                        base_rgb[1],
+                        base_rgb[2]
+                    );
+
+                    // 黒の判定用の閾値（0～255）
+                    var threshold = 180;
+
+                    let rgb_threshold = 150; //小さくすると透明になる面積が多くなる
+
+                    //黒を黒くする
+                    if (shikisa <= rgb_threshold) {
+                        //ピンク以外を表示させる
+                        imgData_black.data[i + 3] = 255;
+                    } else {
+                        //ピンクを透明にする
+                        imgData_black.data[i] = 0;
+                        imgData_black.data[i + 1] = 0;
+                        imgData_black.data[i + 2] = 0;
+                        imgData_black.data[i + 3] = 0;
+                    }
+                }
+                // canvasに変更した画像データを反映する
+                ctx.putImageData(imgd, 0, 0);
+                // canvasのデータURLをimgのsrc属性に設定する
+                img.src = canvas.toDataURL();
+                document.getElementById("view").appendChild(img);
             };
-            fileReader.readAsText(file);
-        }
+        };
+        // ファイルをデータURLとして読み込む
+        fileReader.readAsDataURL(file);
     });
+
+    //画像を作る
+    for (let j = 1; j < 3; j++) {
+        // 画像を読み込む
+        var image = new Image();
+        console.log(j + j * j);
+        image.src = j + ".png"; //.jpgでも.pngでも対応
+
+        // 画像をBlobオブジェクトに変換
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", image.src, true);
+        xhr.responseType = "blob";
+        xhr.onload = function () {
+            console.log(j);
+            var blob = this.response;
+            var reader = new FileReader();
+            reader.onloadend = function () {
+                var dataURL = reader.result;
+                var canvas_black = document.createElement("canvas");
+                var canvas_white = document.createElement("canvas");
+                var ctx_black = canvas_black.getContext("2d");
+                var ctx_white = canvas_white.getContext("2d");
+                canvas_black.width = image.width;
+                canvas_white.width = image.width;
+                canvas_black.height = image.height;
+                canvas_white.height = image.height;
+
+                // 画像を読み込んだ後にdrawImageメソッドを呼び出すように変更
+                image.onload = function () {
+                    ctx_black.drawImage(image, 0, 0);
+                    ctx_white.drawImage(image, 0, 0);
+                    var imgData_black = ctx_black.getImageData(
+                        0,
+                        0,
+                        canvas_black.width,
+                        canvas_black.height
+                    );
+                    var imgData_white = ctx_white.getImageData(
+                        0,
+                        0,
+                        canvas_white.width,
+                        canvas_white.height
+                    );
+
+                    //灰色は白くする
+
+                    for (var i = 0; i < imgData_black.data.length; i += 4) {
+                        // RGBの値を取得
+                        var r = imgData_black.data[i];
+                        var g = imgData_black.data[i + 1];
+                        var b = imgData_black.data[i + 2];
+
+                        //0は透明
+
+                        //黒レイヤーの操作
+                        //黒は黒くする 黒以外は透明にする
+
+                        //if (r >= threshold && g >= threshold && b >= threshold) {
+
+                        function euclidean(r1, g1, b1, r2, g2, b2) {
+                            return Math.sqrt(
+                                Math.pow(r2 - r1, 2) +
+                                Math.pow(g2 - g1, 2) +
+                                Math.pow(b2 - b1, 2)
+                            );
+                        }
+                        //白の値は360付近
+
+                        let base_rgb = [51, 16, 36];
+
+                        let shikisa = euclidean(
+                            imgData_black.data[i],
+                            imgData_black.data[i + 1],
+                            imgData_black.data[i + 2],
+                            base_rgb[0],
+                            base_rgb[1],
+                            base_rgb[2]
+                        );
+
+                        // 黒の判定用の閾値（0～255）
+                        var threshold = 180;
+
+                        let rgb_threshold = 150; //小さくすると透明になる面積が多くなる
+
+                        //黒を黒くする
+                        if (shikisa <= rgb_threshold) {
+                            //ピンク以外を表示させる
+                            imgData_black.data[i + 3] = 255;
+                        } else {
+                            //ピンクを透明にする
+                            imgData_black.data[i] = 0;
+                            imgData_black.data[i + 1] = 0;
+                            imgData_black.data[i + 2] = 0;
+                            imgData_black.data[i + 3] = 0;
+                        }
+
+                        //黒を黒くする
+                        // if (r <= threshold && b <= threshold) {
+                        //   //黒を黒くする
+                        //   imgData_black.data[i + 3] = 255;
+                        // } else {
+                        //   //黒以外を透明にする
+                        //   imgData_black.data[i] = 0;
+                        //   imgData_black.data[i + 1] = 0;
+                        //   imgData_black.data[i + 2] = 0;
+                        //   imgData_black.data[i + 3] = 0;
+                        // }
+
+                        //黒を白くする  理想は、灰色(jpgのノイズ)を白にする
+                        // if (r >= threshold && g >= threshold && b >= threshold) {
+                        //   continue;
+                        // } else {
+                        // imgData_white.data[i] = 255;
+                        // imgData_white.data[i + 1] = 255;
+                        // imgData_white.data[i + 2] = 255;
+                        // imgData_white.data[i + 3] = 255; //0にすると黒が透明になる
+                        //}
+                    }
+
+                    ctx_black.putImageData(imgData_black, 0, 0);
+                    ctx_white.putImageData(imgData_white, 0, 0);
+
+                    resultImg = new Image();
+                    resultImg.src = canvas_white.toDataURL();
+                    console.log(j);
+                    console.log(100 * j + " px");
+                    resultImg.style.top = 100 * j + "px";
+                    resultImg.id = "page-1_background";
+                    document.getElementById("view").appendChild(resultImg);
+
+                    var resultImg = new Image();
+                    resultImg.src = canvas_black.toDataURL();
+                    resultImg.style.top = 100 * j + "px";
+                    resultImg.id = "page-1_black";
+                    document.getElementById("view").appendChild(resultImg);
+                };
+                image.src = dataURL;
+            };
+            reader.readAsDataURL(blob);
+        };
+        xhr.send();
+    }
+
+    //赤シートを作る
+    let html_red_sheet_truth = document.createElement("div");
+    html_red_sheet_truth.id = "redsheet-truth";
+    document.getElementById("view").appendChild(html_red_sheet_truth);
+
+    let html_red_sheet_false = document.createElement("div");
+    html_red_sheet_false.id = "redsheet-false";
+    document.getElementById("view").appendChild(html_red_sheet_false);
+
 
     //赤シートを移動させる
     //要素の取得
@@ -106,147 +342,6 @@ function first_script() {
     }
 }
 
-
-// 画像を読み込む
-var image = new Image();
-image.src = "1.png"; //.jpgでも.pngでも対応
-
-// 画像をBlobオブジェクトに変換
-var xhr = new XMLHttpRequest();
-xhr.open("GET", image.src, true);
-xhr.responseType = "blob";
-xhr.onload = function () {
-    var blob = this.response;
-    var reader = new FileReader();
-    reader.onloadend = function () {
-        var dataURL = reader.result;
-        var canvas_black = document.createElement("canvas");
-        var canvas_white = document.createElement("canvas");
-        var ctx_black = canvas_black.getContext("2d");
-        var ctx_white = canvas_white.getContext("2d");
-        canvas_black.width = image.width;
-        canvas_white.width = image.width;
-        canvas_black.height = image.height;
-        canvas_white.height = image.height;
-
-        // 画像を読み込んだ後にdrawImageメソッドを呼び出すように変更
-        image.onload = function () {
-            ctx_black.drawImage(image, 0, 0);
-            ctx_white.drawImage(image, 0, 0);
-            var imgData_black = ctx_black.getImageData(
-                0,
-                0,
-                canvas_black.width,
-                canvas_black.height
-            );
-            var imgData_white = ctx_white.getImageData(
-                0,
-                0,
-                canvas_white.width,
-                canvas_white.height
-            );
-
-            //灰色は白くする
-
-            for (var i = 0; i < imgData_black.data.length; i += 4) {
-                // RGBの値を取得
-                var r = imgData_black.data[i];
-                var g = imgData_black.data[i + 1];
-                var b = imgData_black.data[i + 2];
-
-                //0は透明
-
-                //黒レイヤーの操作
-                //黒は黒くする 黒以外は透明にする
-
-                //if (r >= threshold && g >= threshold && b >= threshold) {
-
-                function euclidean(r1, g1, b1, r2, g2, b2) {
-                    return Math.sqrt(
-                        Math.pow(r2 - r1, 2) +
-                        Math.pow(g2 - g1, 2) +
-                        Math.pow(b2 - b1, 2)
-                    );
-                }
-                //白の値は360付近
-
-                let base_rgb = [51, 16, 36];
-
-                let shikisa = euclidean(
-                    imgData_black.data[i],
-                    imgData_black.data[i + 1],
-                    imgData_black.data[i + 2],
-                    base_rgb[0],
-                    base_rgb[1],
-                    base_rgb[2]
-                );
-
-                // 黒の判定用の閾値（0～255）
-                var threshold = 180;
-
-                let rgb_threshold = 150; //小さくすると透明になる面積が多くなる
-
-                //黒を黒くする
-                if (shikisa <= rgb_threshold) {
-                    //ピンク以外を表示させる
-                    imgData_black.data[i + 3] = 255;
-                } else {
-                    //ピンクを透明にする
-                    imgData_black.data[i] = 0;
-                    imgData_black.data[i + 1] = 0;
-                    imgData_black.data[i + 2] = 0;
-                    imgData_black.data[i + 3] = 0;
-                }
-
-                //黒を黒くする
-                // if (r <= threshold && b <= threshold) {
-                //   //黒を黒くする
-                //   imgData_black.data[i + 3] = 255;
-                // } else {
-                //   //黒以外を透明にする
-                //   imgData_black.data[i] = 0;
-                //   imgData_black.data[i + 1] = 0;
-                //   imgData_black.data[i + 2] = 0;
-                //   imgData_black.data[i + 3] = 0;
-                // }
-
-                //黒を白くする  理想は、灰色(jpgのノイズ)を白にする
-                // if (r >= threshold && g >= threshold && b >= threshold) {
-                //   continue;
-                // } else {
-                // imgData_white.data[i] = 255;
-                // imgData_white.data[i + 1] = 255;
-                // imgData_white.data[i + 2] = 255;
-                // imgData_white.data[i + 3] = 255; //0にすると黒が透明になる
-                //}
-            }
-
-            ctx_black.putImageData(imgData_black, 0, 0);
-            ctx_white.putImageData(imgData_white, 0, 0);
-
-            resultImg = new Image();
-            resultImg.src = canvas_white.toDataURL();
-            resultImg.id = "page-1_background";
-            document.getElementById("view").appendChild(resultImg);
-
-            let html_red_sheet_truth = document.createElement("div");
-            html_red_sheet_truth.id = "redsheet-truth";
-            document.getElementById("view").appendChild(html_red_sheet_truth);
-
-            let html_red_sheet_false = document.createElement("div");
-            html_red_sheet_false.id = "redsheet-false";
-            document.getElementById("view").appendChild(html_red_sheet_false);
-
-            var resultImg = new Image();
-            resultImg.src = canvas_black.toDataURL();
-            resultImg.id = "page-1_black";
-            document.getElementById("view").appendChild(resultImg);
-        };
-        image.src = dataURL;
-    };
-    reader.readAsDataURL(blob);
-};
-xhr.send();
 /*
 // 画像を読み込む
 var image = new Image();
